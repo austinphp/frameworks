1b57966bfc612d2fcf24a366074626620d41fc24
Jeff Winesett <jeff.winesett@controlgroup.com>
2011-06-19 17:47:31 -0400

    adding in initial codebase for yii blog site

---
+++ blog/index.php
@@ -0,0 +1,10 @@
+<?php
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../yii-1.1.7/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following line when in production mode
+// defined('YII_DEBUG') or define('YII_DEBUG',true);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
